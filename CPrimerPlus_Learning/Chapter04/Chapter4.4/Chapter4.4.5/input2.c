5c298070228e43eb2a4399fb551e95d6c1678832
ryan0028 <ryan0028@student.monash.edu>
2023-01-27 08:22:24 +1100

    Chapter4.4.5 scanf() method update

---
+++ CPrimerPlus_Learning/Chapter04/Chapter4.4/Chapter4.4.5/input2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    int age;
+    scanf("%5d", &age); // 读取5个字段
+    printf("age = %d\n", age);
+    return 0;
+}
---
+++ CPrimerPlus_Learning/Chapter04/Chapter4.4/Chapter4.4.5/input3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+//    int age;
+//    int num;
+//    scanf("%d %d", &age,&num);
+//    printf("age=%d num=%d", age, num);
+
+    char name[40];
+    scanf("%s", name);
+    printf("name = %s\n", name);
+    return 0;
+}
