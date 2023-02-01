ae22e5f11fa7617b57f9eb31d16a9e4b1a61e93d
ryan0028 <ryan0028@student.monash.edu>
2023-02-01 19:09:56 +1100

    update to for loops

--- OnlineClass/Class_6/Loops/For/for.c
+++ OnlineClass/Class_6/Loops/For/for.c
@@ -6,5 +6,19 @@ int main(void) {
     for (int i = 1; i <= 10; i++) {
         printf("%d ", i);
     }
+    printf("\n");
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+            break;
+        }
+        printf("%d ", i);
+    }
+    printf("\n");
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+            continue;
+        }
+        printf("%d ", i);
+    }
     return 0;
 }
